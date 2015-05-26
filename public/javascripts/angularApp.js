7a9f802730d6f5499591450eeee28fca8b508000
Matthew Gibbs <mygibbs@gmail.com>
2015-05-26 16:15:58 -0400

    finished comments, fixed upvoting of posts

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -19,7 +19,12 @@ function($stateProvider, $urlRouterProvider){
 		.state('posts', {
 			url: '/posts/{id}',
 			templateUrl: '/posts.html',
-			controller: 'PostsCtrl'
+			controller: 'PostsCtrl',
+			resolve: {
+				post: ['$stateParams', 'posts', function($stateParams, posts) {
+					return posts.get($stateParams.id);
+				}]
+			}
 		});
 		
 	$urlRouterProvider.otherwise('home');
@@ -46,8 +51,25 @@ app.factory('posts', ['$http', function($http){
 		return $http.put('/posts/' + post._id + '/upvote')
 			.success(function(data) {
 				post.upvotes += 1;
-			}
-	}
+			});
+	};
+	
+	o.get = function(id) {
+		return $http.get('/posts/' + id).then(function(res) {
+			return res.data;
+		});
+	};
+	
+	o.addComment = function(id, comment) {
+		return  $http.post('/posts/' + id + '/comments', comment);
+	};
+	
+	o.upvoteComment = function(post, comment) {
+		return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
+			.success(function(data) {
+				comment.upvotes += 1;
+			});
+	};
 	
 	return o;
 }]);
@@ -67,27 +89,32 @@ function($scope, posts){
 		$scope.title = '';
 		$scope.link = '';
 	};
-
+	
 	$scope.incrementUpvotes = function(post){
-		post.upvote(post);
+		posts.upvote(post);
 	}
-
+	
 }]);
 
 app.controller('PostsCtrl', [
 '$scope',
-'$stateParams',
 'posts',
-function($scope, $stateParams, posts){
-	$scope.post = posts.posts[$stateParams.id];
+'post',
+function($scope, posts, post){
+	$scope.post = post;
 	
 	$scope.addComment = function(){
 		if($scope.body === '') { return; }
-		$scope.post.comments.push({
+		posts.addComment(post._id, {
 			body: $scope.body,
-			author: 'user',
-			upvotes: 0
+			author: 'user'
+		}).success(function(comment) {
+			$scope.post.comments.push(comment);
 		});
 		$scope.body = '';
+	};
+	
+	$scope.incrementUpvotes = function(comment) {
+		posts.upvoteComment(post, comment);
 	};
 }]);
