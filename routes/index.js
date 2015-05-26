80f3d5f65408c485c9ef199c34ffa8def78b2432
Matthew Gibbs <mygibbs@gmail.com>
2015-05-26 16:59:12 -0400

    added login route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var passport = require('passport');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -99,6 +101,7 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
 	});
 });
 
+/* POST register user */
 router.post('/register', function(req, res, next) {
 	if(!req.body.username || !req.body.password) {
 		return res.status(400).json({message: 'Please fill out all fields'});
@@ -115,6 +118,23 @@ router.post('/register', function(req, res, next) {
 		
 		return res.json({token: user.generateJWT()});
 	});
+});
+
+/* POST login */
+router.post('/login', function(req, res, next) {
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).json({message: 'Please fill out all fields.'});
+	}
+	
+	passport.authenticate('local', function(err, user, info) {
+		if(err) { return next(err); }
+		
+		if(user) {
+			return res.json({token: user.generateJWT()});
+		} else {
+			return res.status(401).json(info);
+		}
+	})(req, res, next);
 });
 
 module.exports = router;
