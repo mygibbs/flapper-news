0544561662de103965f0557a03a105bf111486f1
Matthew Gibbs <mygibbs@gmail.com>
2015-06-01 10:56:36 -0400

    routing now has auth, author of post and comment now can save

--- routes/index.js
+++ routes/index.js
@@ -23,9 +23,9 @@ router.get('/posts', function(req, res, next) {
 });
 
 /* POST save post */
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
 	var post = new Post(req.body);
-	
+	post.author = req.payload.username;
 	post.save(function(err, post) {
 		if(err) { return next(err); }
 		
@@ -56,7 +56,7 @@ router.get('/posts/:post', function(req, res) {
 });
 
 /* PUT upvote post */
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
 	req.post.upvote(function(err, post) {
 		if(err) { return next(err); }
 		
@@ -65,10 +65,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 });
 
 /* POST create comment */
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
 	var comment = new Comment(req.body);
 	comment.post = req.post;
-	
+	comment.author = req.payload.username;
 	comment.save(function(err, comment) {
 		if(err) { return next(err); }
 		
@@ -95,7 +95,7 @@ router.param('comment', function(req, res, next, id) {
 });
 
 /* PUT upvote comment */
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
 	req.comment.upvote(function(err, comment) {
 		if(err) { return next(err); }
 		
